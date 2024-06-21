1fc7d4d629d999944c30d1cb24395cae2f31bae3
IQ72 <105931001+raihanfaiq72@users.noreply.github.com>
2024-06-21 08:51:09 +0700

    update

--- buku-machine-learning-dengan-python-hana-arini/BAB I/studikasusdiabetes.py
+++ buku-machine-learning-dengan-python-hana-arini/BAB I/studikasusdiabetes.py
@@ -30,5 +30,36 @@ print("===============================================")
 print("ini adalah langkah 5 , Menampilkan data perfaktor")
 faktor = diabetes.groupby('Luaran').hist(figsize=(9,9))
 # faktor = diabetes.hist(figsize=(9,9))
+# plt.show()
 print(faktor)
+print("===============================================")
+
+print("===============================================")
+# memastikan tidak ada missing data 
+print("langkah 6 memastikan tidak ada missing data")
+print(diabetes.isnull().sum())
+print(diabetes.isna().sum())
+print("===============================================")
+
+
+print("===============================================")
+# dari data diketahui jika terdapat sekitar 35 orang bertekanan darah 0 dimana tidak mungkin seseorang bertekanan darah 0
+# proses ini akan menelusuri orang yang bertekanan darah 0
+print("langkah 7 , Menelusuri orang dengan tekanan darah 0")
+tekanan_darah_nol = diabetes[diabetes['TekananDarah'] == 0]
+tekanan_darah_nol_jumlah = diabetes[diabetes['TekananDarah'] == 0].shape[0]
+tekanan_darah_nol_grouped = tekanan_darah_nol.groupby('Luaran').size()
+print(tekanan_darah_nol_grouped)
+print("Total",tekanan_darah_nol_jumlah)
+print("===============================================")
+
+
+print("===============================================")
+# level glukosa plasma , meskipun seseorang berpuasa tidak mungkin glukosa plasma menjadi 0 ini pasti ada kesalahan data
+print("Langkah 8 , orang dengan glukosa plasma 0")
+glukosa_plasma_nol = diabetes[diabetes['Glukosa']==0]
+glukosa_plasma_nol_jumlah = diabetes[diabetes['Glukosa']==0].shape[0]
+glukosa_plasma_nol_gruped = glukosa_plasma_nol.groupby('Luaran')['Umur'].count() # banyak cara , catatan baru
+print(glukosa_plasma_nol_gruped)
+print("Total",glukosa_plasma_nol_jumlah)
 print("===============================================")
